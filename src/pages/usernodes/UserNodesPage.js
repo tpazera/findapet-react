717170fb6cad99520013a3a89ddbe9c5d8286a96
Tomasz Pazera <tomasz.pazera96@gmail.com>
2019-05-06 21:29:06 +0200

    sort bar

--- src/pages/usernodes/UserNodesPage.js
+++ src/pages/usernodes/UserNodesPage.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import PageWrapper from "../../components/pageWrapper/PageWrapper";
+import SidebarMenu from "../../components/sidebarMenu/SidebarMenu";
+import PageContent from "../../components/pageContent/PageContent";
+import NodeList from "../../components/nodeList/NodeList";
+import SortBar from "../../components/sortBar/SortBar"
+import nodes from "../../resources/data.json";
 
 class UserNodesPage extends Component {
     render() {
         return (
-            <div>UserNodes Page</div>
+            <PageWrapper>
+                <SidebarMenu />
+                <PageContent>
+                    <NodeList list={nodes.nodeList} />
+                </PageContent>
+            </PageWrapper>
         );
     }
 }
