e594e80edda35a87033acc5204929eaf40a4109d
Tomasz Pazera <tomasz.pazera96@gmail.com>
2019-05-13 14:14:44 +0200

    fix notification header

--- src/pages/usernodes/UserNodesPage.js
+++ src/pages/usernodes/UserNodesPage.js
@@ -3,7 +3,6 @@ import PageWrapper from "../../components/pageWrapper/PageWrapper";
 import SidebarMenu from "../../components/sidebarMenu/SidebarMenu";
 import PageContent from "../../components/pageContent/PageContent";
 import NodeList from "../../components/nodeList/NodeList";
-import SortBar from "../../components/sortBar/SortBar"
 import nodes from "../../resources/data.json";
 
 class UserNodesPage extends Component {
