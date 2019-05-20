acf761505dda792a54e596f461779cca1c0484cc
ethArek <arkadiusz.sidzinka@gmail.com>
2019-05-21 00:35:40 +0200

    redux store config and filtering announcements

--- src/components/leafletMap/LeafletMap.js
+++ src/components/leafletMap/LeafletMap.js
@@ -1,129 +1,140 @@
-import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import MarkerClusterGroup from "../markerClusterGroup/MarkerClusterGroup";
-import './leafletMap.scss';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-
+import "./leafletMap.scss";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { addCoordinates } from "../../modules/store/actions/coordinate";
 
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
 });
 
 const catMarker = L.icon({
-    iconUrl: '/images/cat.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
+  iconUrl: "/images/cat.svg",
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
 });
 
 const dogMarker = L.icon({
-    iconUrl: '/images/dog.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
+  iconUrl: "/images/dog.svg",
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
 });
 
 const defaultMarker = L.icon({
-    iconUrl: '/images/marker.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
+  iconUrl: "/images/marker.svg",
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
 });
 
 const bunnyMarker = L.icon({
-    iconUrl: '/images/bunny.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
+  iconUrl: "/images/bunny.svg",
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
 });
 
 const parrotMarker = L.icon({
-    iconUrl: '/images/parrot.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
+  iconUrl: "/images/parrot.svg",
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
 });
 
 const mouseMarker = L.icon({
-    iconUrl: '/images/mouse.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
+  iconUrl: "/images/mouse.svg",
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
 });
 
-
 class LeafletMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            lat: 51.505,
-            lng: -0.09,
-            zoom: 13
-        }
+  saveLocationInfo(position) {
+    const { longitude, latitude } = position.coords;
+    this.setState({
+      lng: longitude,
+      lat: latitude,
+      zoom: 13
+    });
+
+    // console.log(`longitude: ${this.state.lng} | latitude: ${this.state.lat}`);
+  }
+
+  handleMoveend = ev => {
+    //this.saveLocationInfo(ev.sourceTarget.getBounds());
+    const { _northEast, _southWest } = ev.sourceTarget.getBounds();
+    this.props.addCoordinates(_northEast, _southWest);
+  };
+
+  render() {
+    const { list } = this.props;
+
+    let dogs = list.filter(item => item.animalType === "pies");
+
+    let cats = list.filter(item => item.animalType === "kot");
+
+    let bunnies = list.filter(item => item.animalType === "królik");
+
+    let mouses = list.filter(item => item.animalType === "mysz");
+
+    let parrots = list.filter(item => item.animalType === "papuga");
+
+    let others = list.filter(
+      item =>
+        item.animalType !== "pies" &&
+        item.animalType !== "kot" &&
+        item.animalType !== "mysz" &&
+        item.animalType !== "królik" &&
+        item.animalType !== "papuga"
+    );
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this.saveLocationInfo.bind(this)
+      );
     }
 
-    saveLocationInfo(position) {
-        this.setState({
-            lng: position.coords.longitude,
-            lat: position.coords.latitude,
-            zoom: 13
-        });
-      
-        console.log(`longitude: ${ this.state.lng } | latitude: ${ this.state.lat }`);
-    }
+    const isListNotEmpty = list && list.length > 0;
 
-    handleMoveend(ev) {
-        console.log(ev.sourceTarget.getBounds());
-    }
+    let position = [this.state.lat, this.state.lng];
 
-    render() {
-
-        const {
-            list
-        } = this.props;
-
-        let dogs = list.filter(
-            item => item.animalType === "pies"
-        ); 
-
-        let cats = list.filter(
-            item => item.animalType === "kot"
-        ); 
-
-        let bunnies = list.filter(
-            item => item.animalType === "królik"
-        ); 
-
-        let mouses = list.filter(
-            item => item.animalType === "mysz"
-        ); 
-
-        let parrots = list.filter(
-            item => item.animalType === "papuga"
-        ); 
-
-        let others = list.filter(
-            item => (item.animalType !== "pies" && item.animalType !== "kot" && item.animalType !== "mysz" && item.animalType !== "królik" && item.animalType !== "papuga")
-        ); 
-
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this.saveLocationInfo.bind(this));
-        }
-
-
-        const isListNotEmpty = list && list.length > 0;
-
-        let position = [this.state.lat, this.state.lng]
-        
-        return (
-            <Map onMoveend={this.handleMoveend} center={position} zoom={this.state.zoom} id="map" maxZoom={25}>
-                <TileLayer
-                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <MarkerClusterGroup>
-                    {/* {isListNotEmpty && list.map((node) => (
+    return (
+      <>
+        <button
+          onClick={() =>
+            this.saveLocationInfo({
+              coords: { latitude: 32, longitude: 32 }
+            })
+          }
+        >
+          asdsA
+        </button>
+        <Map
+          onMoveend={this.handleMoveend}
+          center={position}
+          zoom={this.state.zoom}
+          id="map"
+          maxZoom={25}
+        >
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <MarkerClusterGroup>
+            {/* {isListNotEmpty && list.map((node) => (
                         <Marker key={node.id} position={[node.lat, node.lng]}>
                                 <Popup>
                                     <p>{node.title}</p>
@@ -131,72 +142,97 @@ class LeafletMap extends Component {
                                 </Popup>
                         </Marker>
                     ))} */}
-                    {isListNotEmpty && others.map((node) => (
-                        <Marker key={node.id} position={[node.lat, node.lng]}
-                            icon={defaultMarker}
-                        >
-                                <Popup>
-                                    <p>{node.title}</p>
-                                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
-                                </Popup>
-                        </Marker>
-                    ))}
-                    {isListNotEmpty && dogs.map((node) => (
-                        <Marker key={node.id} position={[node.lat, node.lng]}
-                            icon={dogMarker}
-                        >
-                                <Popup>
-                                    <p>{node.title}</p>
-                                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
-                                </Popup>
-                        </Marker>
-                    ))}
-                    {isListNotEmpty && cats.map((node) => (
-                        <Marker key={node.id} position={[node.lat, node.lng]}
-                            icon={catMarker}
-                        >
-                                <Popup>
-                                    <p>{node.title}</p>
-                                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
-                                </Popup>
-                        </Marker>
-                    ))}
-                    {isListNotEmpty && bunnies.map((node) => (
-                        <Marker key={node.id} position={[node.lat, node.lng]}
-                            icon={bunnyMarker}
-                        >
-                                <Popup>
-                                    <p>{node.title}</p>
-                                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
-                                </Popup>
-                        </Marker>
-                    ))}
-                    {isListNotEmpty && mouses.map((node) => (
-                        <Marker key={node.id} position={[node.lat, node.lng]}
-                            icon={mouseMarker}
-                        >
-                                <Popup>
-                                    <p>{node.title}</p>
-                                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
-                                </Popup>
-                        </Marker>
-                    ))}
-                    {isListNotEmpty && parrots.map((node) => (
-                        <Marker key={node.id} position={[node.lat, node.lng]}
-                            icon={parrotMarker}
-                        >
-                                <Popup>
-                                    <p>{node.title}</p>
-                                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
-                                </Popup>
-                        </Marker>
-                    ))}
-                </MarkerClusterGroup>
-
-                
-            </Map>
-        )
-    }
+            {isListNotEmpty &&
+              others.map(node => (
+                <Marker
+                  key={node.id}
+                  position={[node.lat, node.lng]}
+                  icon={defaultMarker}
+                >
+                  <Popup>
+                    <p>{node.title}</p>
+                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
+                  </Popup>
+                </Marker>
+              ))}
+            {isListNotEmpty &&
+              dogs.map(node => (
+                <Marker
+                  key={node.id}
+                  position={[node.lat, node.lng]}
+                  icon={dogMarker}
+                >
+                  <Popup>
+                    <p>{node.title}</p>
+                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
+                  </Popup>
+                </Marker>
+              ))}
+            {isListNotEmpty &&
+              cats.map(node => (
+                <Marker
+                  key={node.id}
+                  position={[node.lat, node.lng]}
+                  icon={catMarker}
+                >
+                  <Popup>
+                    <p>{node.title}</p>
+                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
+                  </Popup>
+                </Marker>
+              ))}
+            {isListNotEmpty &&
+              bunnies.map(node => (
+                <Marker
+                  key={node.id}
+                  position={[node.lat, node.lng]}
+                  icon={bunnyMarker}
+                >
+                  <Popup>
+                    <p>{node.title}</p>
+                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
+                  </Popup>
+                </Marker>
+              ))}
+            {isListNotEmpty &&
+              mouses.map(node => (
+                <Marker
+                  key={node.id}
+                  position={[node.lat, node.lng]}
+                  icon={mouseMarker}
+                >
+                  <Popup>
+                    <p>{node.title}</p>
+                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
+                  </Popup>
+                </Marker>
+              ))}
+            {isListNotEmpty &&
+              parrots.map(node => (
+                <Marker
+                  key={node.id}
+                  position={[node.lat, node.lng]}
+                  icon={parrotMarker}
+                >
+                  <Popup>
+                    <p>{node.title}</p>
+                    <Link to={"/n/" + node.id}>KLIKNIJ</Link>
+                  </Popup>
+                </Marker>
+              ))}
+          </MarkerClusterGroup>
+        </Map>
+      </>
+    );
+  }
 }
 
-export default LeafletMap;
+const mapDispatchToProps = dispatch => ({
+  addCoordinates: (latitude, longitude) =>
+    dispatch(addCoordinates(latitude, longitude))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(LeafletMap);
--- src/modules/router/AppRouter.js
+++ src/modules/router/AppRouter.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 import NodeListPage from "./../../pages/nodelist/NodeListPage";
 import NodePage from "./../../pages/node/NodePage";
 import UserNodesPage from "./../../pages/usernodes/UserNodesPage";
 import AddAnnounecement from "../../pages/announecements/AddAnnounecement";
 import AboutPage from "./../../pages/about/AboutPage";
 import ErrorPage from "./../../pages/error/ErrorPage";
+import configureStore from "../store/store";
+
+const store = configureStore();
 
 const AppRouter = ({ children }) => (
-  <BrowserRouter>
-    <div>
-      {children}
-      <Switch>
-        <Route path="/" exact component={NodeListPage} />
-        <Route path="/n/:id(\d+)" component={NodePage} />
-        <Route path="/twoje-ogloszenia" component={UserNodesPage} />
-        <Route path="/dodaj-ogloszenie" component={AddAnnounecement} />
-        <Route path="/o-nas" component={AboutPage} />
-        <Route component={ErrorPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        {children}
+        <Switch>
+          <Route path="/" exact component={NodeListPage} />
+          <Route path="/n/:id(\d+)" component={NodePage} />
+          <Route path="/twoje-ogloszenia" component={UserNodesPage} />
+          <Route path="/dodaj-ogloszenie" component={AddAnnounecement} />
+          <Route path="/o-nas" component={AboutPage} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default AppRouter;
