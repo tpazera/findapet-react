1ca6ecc31c923c71c0ec91035e91e9e7d62b25af
lurat <40599795+lurat@users.noreply.github.com>
2019-06-10 19:13:05 +0200

    AboutPage zmiany

--- src/pages/about/AboutPage.js
+++ src/pages/about/AboutPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PageWrapper from "../../components/pageWrapper/PageWrapper";
 import Sidebar from "../../components/sidebar/Sidebar";
 import PageContent from "../../components/pageContent/PageContent";
+import Logo from "../../components/logo/Logo";
 import ContentHeader from "../../components/contentHeader/ContentHeader";
 import './aboutPage.scss';
 
@@ -12,16 +13,13 @@ class AboutPage extends Component {
                 <Sidebar />
                 <PageContent>
                     <ContentHeader>O nas</ContentHeader>
+                    <Logo id="logo"></Logo>
                     <div id="aboutContent">
-                    Sed hendrerit. Vivamus laoreet. Quisque id odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-
-Quisque id mi. Praesent venenatis metus at tortor pulvinar varius. Cras non dolor. Fusce convallis metus id felis luctus adipiscing.
-
-Morbi mollis tellus ac sapien. Vestibulum volutpat pretium libero. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Proin magna.
-
-Cras varius. Phasellus viverra nulla ut metus varius laoreet. Vivamus elementum semper nisi. Vestibulum volutpat pretium libero.
-
-Sed fringilla mauris sit amet nibh. Praesent nec nisl a purus blandit viverra. Phasellus dolor. Praesent adipiscing.
+                    <strong><p>PAWFINDER jest to aplikacja stworzona z myślą o posiadaczach róznego rodzaju zwierząt. Ma ona na celu umożliwienie właścicielowi na utworzenie zgłoszenia
+                    o zaginięciu ich ukochanego pupila. Każda osoba, która zobaczy zwierzę bezpańsko poruszające się po okolicy jest wstanie w naszej aplikacji to zgłosić. Jezeli 
+                    zwierzę to będzie pasowało do opisu podanego w czyimś zgłoszeniu może o tym poinformować twórcę tego zgłoszenia, a następnie się z nim skontaktować, aby pomóc mu
+                    w odnalezieniu ukochanego zwierzęcia. Mamy nadzieję, ze nasza aplikacja pomoże jak największej ilości osób oraz także i tobie w odnalezieniu swojego zagionego 
+                    zwierzaka!</p></strong>
                     </div>
                 </PageContent>
             </PageWrapper>
